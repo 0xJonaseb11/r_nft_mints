d421d66446515bb544d5c650472eeef2b042c30c
jonas-sebera <sebejaz99@gmail.com>
2023-12-15 17:34:57 +0200

    update fix

--- web3/contracts/MyNFT.sol
+++ web3/contracts/MyNFT.sol
@@ -1,52 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import { ERC721URIStorageUpgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import { UUPSUpgradeable } from "openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.aol";
-
+import "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@5.0.1/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/UUPSUpgradeable.sol";
 
 contract FiredGuys is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 private _nextTokenId;
 
-    constructor () {
-        _disableInitializer();
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
     function initialize(address initialOwner) initializer public {
-        __ERC721_init("FireGuys", FYR);
+        __ERC721_init("FiredGuys", "FYR");
         __ERC721URIStorage_init();
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
     }
 
-    function _baseURI() internal pure override returns(string memory) {
+    function _baseURI() internal pure override returns (string memory) {
         return "ipfs://";
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _nextTokenId;
+        uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    function _autorizeUpgrade(address newImplementation) 
-    internal onlyOwner override {}
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
 
-    // The following funtions are overrides required by solidity
+    // The following functions are overrides required by Solidity.
 
     function tokenURI(uint256 tokenId)
-    public view 
-    override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
-    returns (string memory) {
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterfaces(bytes4 interfaceId) 
-    public view override(ERC721Upgradeable, ERC721StorageUpgradeable)
-    returns (bool) {
-        return super.supportsInterfaces(interfaceId);
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }

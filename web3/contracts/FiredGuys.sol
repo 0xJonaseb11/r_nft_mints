ebc88fd10a2d730562e085b88be53cb8d3a67b25
jonas-sebera <sebejaz99@gmail.com>
2023-12-15 21:45:44 +0200

    update fixes

--- web3/contracts/FiredGuys.sol
+++ web3/contracts/FiredGuys.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721URIStorage } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Counter } from "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @custom:security-contact sebejaz99@gmail.com
 contract FiredGuys is ERC721, ERC721URIStorage, Ownable {

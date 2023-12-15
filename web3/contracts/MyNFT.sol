14f3f85a6b33a4e67442568e57c517b55e664c0e
jonas-sebera <sebejaz99@gmail.com>
2023-12-15 17:07:23 +0200

    imports init

--- web3/contracts/MyNFT.sol
+++ web3/contracts/MyNFT.sol
@@ -3,3 +3,6 @@ pragma solidity ^0.8.19;
 
 import { ERC721Upgaradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.aol";

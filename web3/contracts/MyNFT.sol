87f7fd830b3ddedba1fcf2f465796170a5dcce8b
jonas-sebera <sebejaz99@gmail.com>
2023-12-15 17:02:15 +0200

    contact update

--- web3/contracts/MyNFT.sol
+++ web3/contracts/MyNFT.sol
@@ -1,1 +1,5 @@
-// 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC721Upgaradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";

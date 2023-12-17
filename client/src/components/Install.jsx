b4f101baff94893603f079191ce2286906d1d1ef
jonas-sebera <sebejaz99@gmail.com>
2023-12-17 12:44:43 +0200

    init update client

---
+++ client/src/components/Install.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Install = () => {
+  return (
+    <div>
+      <h3>
+        Follow the link below to Install
+      </h3>
+      <a href="https://metamask.io/download.html">Metamask</a>
+    </div>
+  )
+}
+
+export default Install

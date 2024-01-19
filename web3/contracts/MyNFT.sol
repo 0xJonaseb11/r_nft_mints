4e1751c0d76a40f2f55e8d35a673bc20b69ddc50
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-19 19:14:53 +0200

    update

--- web3/contracts/MyNFT.sol
+++ web3/contracts/MyNFT.sol
@@ -1,93 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/ERC721Upgradeable.sol";
-import {ERC721URIStorageUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/access/OwnableUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/UUPSUpgradeable.sol";
-import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+// import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/ERC721Upgradeable.sol";
+// import {ERC721URIStorageUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+// import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/access/OwnableUpgradeable.sol";
+// import {Initializable} from "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/Initializable.sol";
+// import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable@5.0.1/proxy/utils/UUPSUpgradeable.sol";
+// import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
-contract FiredGuys is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+ contract FiredGuys /* is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable*/ {
     uint256 private _nextTokenId;
 
-     using Counters for Counters.Counter;
+    address public minersAddress;
 
-     Counters.Counter private _tokenIdCounter;
+//      using Counters for Counters.Counter;
 
-     mapping(string => uint8) existignURIs;
+//      Counters.Counter private _tokenIdCounter;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
+//      mapping(string => uint8) existignURIs;
 
-    function initialize(address initialOwner) initializer public {
-        __ERC721_init("FiredGuys", "FYR");
-        __ERC721URIStorage_init();
-        __Ownable_init(initialOwner);
-        __UUPSUpgradeable_init();
-    }
+//     /// @custom:oz-upgrades-unsafe-allow constructor
+//     constructor() {
+//         _disableInitializers();
+//     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://";
-    }
+//     function initialize(address initialOwner) initializer public {
+//         __ERC721_init("FiredGuys", "FYR");
+//         __ERC721URIStorage_init();
+//         __Ownable_init(initialOwner);
+//         __UUPSUpgradeable_init();
+//     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
+//     function _baseURI() internal pure override returns (string memory) {
+//         return "ipfs://";
+//     }
 
-    function payToMint(address recipient, strin memory) {}
+//     function safeMint(address to, string memory uri) public onlyOwner {
+//         uint256 tokenId = _nextTokenId++;
+//         _safeMint(to, tokenId);
+//         _setTokenURI(tokenId, uri);
+//     }
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyOwner
-        override
-    {}
+//     function payToMint(address recipient, strin memory) {}
 
-    // The following functions are overrides required by Solidity.
+//     function _authorizeUpgrade(address newImplementation)
+//         internal
+//         onlyOwner
+//         override
+//     {}
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+//     // The following functions are overrides required by Solidity.
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+//     function tokenURI(uint256 tokenId)
+//         public
+//         view
+//         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+//         returns (string memory)
+//     {
+//         return super.tokenURI(tokenId);
+//     }
 
-    //check whether content belongs to another person
-    function isContentOwned(string memory uri)public view returns(bool) {
-        return existignURIs[uri] == 1; // returns true
-    }
+//     function supportsInterface(bytes4 interfaceId)
+//         public
+//         view
+//         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+//         returns (bool)
+//     {
+//         return super.supportsInterface(interfaceId);
+//     }
 
-        function payToMint(address recipient, strin memory) public payable returns(uint256){
-            require(existignURIs[metadataURI] != 1, "NFT has already been minted");
-            require(msg.value >= 0.5 ether, "Need to pay up!");
+//     //check whether content belongs to another person
+//     function isContentOwned(string memory uri)public view returns(bool) {
+//         return existignURIs[uri] == 1; // returns true
+//     }
 
-            uint256 newItemId = _tokenIdCounter.current();
-            _tokenIdCounter.increment();
-            existignURIs[metadataURI] = 1;
+//         function payToMint(address recipient, strin memory) public payable returns(uint256){
+//             require(existignURIs[metadataURI] != 1, "NFT has already been minted");
+//             require(msg.value >= 0.5 ether, "Need to pay up!");
 
-            _mint(recipient, newItemId);
-            _setTokenURI(newItemId, metadataURI);
+//             uint256 newItemId = _tokenIdCounter.current();
+//             _tokenIdCounter.increment();
+//             existignURIs[metadataURI] = 1;
+
+//             _mint(recipient, newItemId);
+//             _setTokenURI(newItemId, metadataURI);
             
-            return newItemId;
-        }
+//             return newItemId;
+//         }
 
-           // gt token count
-        function count() public view returns(uint256) {
-            return _tokenIdCounter.current();
+//            // gt token count
+//         function count() public view returns(uint256) {
+//             return _tokenIdCounter.current();
+//         }
+
+        // Block information
+        function getCurrentBlockMiner(address _minersAddress) public view returns(address) {
+            minersAddress = block.coinbase(address);
+            minersAddress = _minersAddress;
+
+            return _minersAddress;
+
         }
 }
